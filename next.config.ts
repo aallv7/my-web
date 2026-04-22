72e14d35ffb768b2cb0e2b0c4f0c43d1efb63da2
TheAlvi <maulvibebekterbang@gmail.com>
2026-04-22 19:52:25 +0700

    awedrgdfbsdfgv

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  output: 'export', // Enables static export
+  basePath: '/my-web', // Required for repository-based pages
+  images: {
+    unoptimized: true, // GitHub Pages doesn't support Next.js image optimization
+  },
+
 };
 
 export default nextConfig;
